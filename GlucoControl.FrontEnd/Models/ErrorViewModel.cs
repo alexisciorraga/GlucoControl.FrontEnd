8e054435de52f67a21fe8dac25fa2ee8a915b293
Alexis Ciorraga Rodriguez <dark_neock@hotmail.com>
2020-11-14 19:06:52 +0100

    Agregar archivos de proyecto.

---
+++ GlucoControl.FrontEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GlucoControl.FrontEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
